720240cb0c255536d2ae0bc96cc35c08216d8277
Luis Arriaga <arriaga.luis.j@gmail.com>
2026-02-09 23:20:27 -0600

    feat(openapi): document employee endpoints

---
+++ src/main/java/com/arriaga/invex/employeeservice/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.arriaga.invex.employeeservice.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Employee Service API",
+        version = "1.0.0",
+        description = "API for managing employees",
+        contact = @Contact(name = "Invex Platform Team"),
+        license = @License(name = "Proprietary")
+    ),
+    servers = {
+        @Server(url = "http://localhost:8080", description = "Local environment")
+    }
+)
+public class OpenApiConfig {
+}
